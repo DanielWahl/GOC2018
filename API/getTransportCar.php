6ff5f2a44a9054a623ea4edd1de23ede03f5fd27
Alex Vesic <a.veselinovic.38@gmail.com>
2018-03-17 05:48:29 +0100

    some changed to make eric happy

--- API/getTransportCar.php
+++ API/getTransportCar.php
@@ -6,6 +6,8 @@
  * Time: 00:47
  */
 
+header("Content-Type: application/json");
+
 if(!isset($_POST["start_lng"]) || !isset($_POST["start_lat"]) || !isset($_POST["dest_lng"]) || !isset($_POST["dest_lat"])) {
 
     echo "ERROR";
--- API/getTransports.php
+++ API/getTransports.php
@@ -5,6 +5,9 @@
  * Date: 16.03.2018
  * Time: 20:22
  */
+
+header("Content-Type: application/json");
+
 if(!isset($_POST["start_lng"])
     || !isset($_POST["start_lat"])
     || !isset($_POST["dest_lng"])
