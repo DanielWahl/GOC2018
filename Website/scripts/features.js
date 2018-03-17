4a7489b872f98fb9658277fc892cc23f25f3a43a
Daniel Wahl <>
2018-03-17 10:14:03 +0100

    updated person, veloh and car visualization

--- Website/scripts/features.js
+++ Website/scripts/features.js
@@ -45,7 +45,36 @@ function loadValues() {
         console.log(response.veloh[3].duration + "s");
         console.log(response.veloh[3].distance + "m");
 
+        let velohdist = (response.veloh[1].distance + response.veloh[2].distance + response.veloh[3].distance)/1000;
+        let velohtime = (response.veloh[1].duration + response.veloh[2].duration + response.veloh[3].duration)/60;
 
+        let content = "<span id='testresult'></span>"+
+        "<p>"+
+        "Dier braucht mam:<br></p>"+
+        "<table>"+
+        "   <tr>"+
+        "       <th><img src='images/bus.svg' style='height:auto;width:60px'></th>"+
+        "       <td><span id='bus_zeit'></span> min</td>"+
+        "       <td><span id='bus_distance'></span> km</td>"+
+        "   </tr>"+
+        "   <tr>"+
+        "       <th><img src='images/bicycle.svg' style='height:auto;width:60px'></th>"+
+        "       <td><span id='veloh_zeit'></span>" + velohtime + " min</td>"+
+        "       <td><span id='veloh_distance'></span>" + velohdist + " km</td>"+
+        "   </tr>"+
+        "   <tr>"+
+        "       <th><img src='images/auto.svg' style='height:auto;width:60px'></th>"+
+        "       <td><span id='auto_zeit'></span>" + response.car[1].duration/60 + " min</td>"+
+        "       <td><span id='auto_distance'></span>" + response.car[1].distance/1000 + " km</td>"+
+        "   </tr>"+
+        "   <tr>"+
+        "       <th><img src='images/person.svg' style='height:auto;width:60px'></th>"+
+        "       <td><span id='person_zeit'></span>" + response.walk[1].duration/60 + " min</td>"+
+        "       <td><span id='person_distance'></span>" + response.walk[1].distance/1000 + " km</td>"+
+        "   </tr>"+
+        "</table>";
+
+        $('#result').append(content);
 
     };
 
