73a3c18c1b0b727b0b53418bd0a5c7817f607c3a
Daniel Wahl <>
2018-03-17 11:01:33 +0100

    changes on frontend because Alex is sleeping

--- Website/scripts/features.js
+++ Website/scripts/features.js
@@ -26,7 +26,7 @@ function loadValues() {
 
         let response = JSON.parse(e.target.response);
 
-        console.log(response);
+        /*console.log(response);
 
         console.log("Walk");
         console.log(response.walk[1].duration + "s");
@@ -43,10 +43,16 @@ function loadValues() {
         console.log(response.veloh[2].distance + "m");
         console.log("Distance to destination");
         console.log(response.veloh[3].duration + "s");
-        console.log(response.veloh[3].distance + "m");
+        console.log(response.veloh[3].distance + "m");*/
 
-        let velohdist = (response.veloh[1].distance + response.veloh[2].distance + response.veloh[3].distance)/1000;
-        let velohtime = (response.veloh[1].duration + response.veloh[2].duration + response.veloh[3].duration)/60;
+        let busdist     = 0;
+        let bustime     = Math.round(0);
+        let velohdist   = (response.veloh[1].distance + response.veloh[2].distance + response.veloh[3].distance)/1000;
+        let velohtime   = Math.round((response.veloh[1].duration + response.veloh[2].duration + response.veloh[3].duration)/60);
+        let cardist     = response.car[1].distance/1000;
+        let cartime     = Math.round(response.car[1].duration/60);
+        let persondist  = response.walk[1].distance/1000;
+        let persontime  = Math.round(response.walk[1].duration/60);
 
         let content = "<span id='testresult'></span>"+
         "<p>"+
@@ -54,8 +60,8 @@ function loadValues() {
         "<table>"+
         "   <tr>"+
         "       <th><img src='images/bus.svg' style='height:auto;width:60px'></th>"+
-        "       <td><span id='bus_zeit'></span> min</td>"+
-        "       <td><span id='bus_distance'></span> km</td>"+
+        "       <td><span id='bus_zeit'></span>" + bustime + " min</td>"+
+        "       <td><span id='bus_distance'></span>" + busdist + " km</td>"+
         "   </tr>"+
         "   <tr>"+
         "       <th><img src='images/bicycle.svg' style='height:auto;width:60px'></th>"+
@@ -64,13 +70,13 @@ function loadValues() {
         "   </tr>"+
         "   <tr>"+
         "       <th><img src='images/auto.svg' style='height:auto;width:60px'></th>"+
-        "       <td><span id='auto_zeit'></span>" + response.car[1].duration/60 + " min</td>"+
-        "       <td><span id='auto_distance'></span>" + response.car[1].distance/1000 + " km</td>"+
+        "       <td><span id='auto_zeit'></span>" + cartime + " min</td>"+
+        "       <td><span id='auto_distance'></span>" + cardist + " km</td>"+
         "   </tr>"+
         "   <tr>"+
-        "       <th><img src='images/person.svg' style='height:auto;width:60px'></th>"+
-        "       <td><span id='person_zeit'></span>" + response.walk[1].duration/60 + " min</td>"+
-        "       <td><span id='person_distance'></span>" + response.walk[1].distance/1000 + " km</td>"+
+        "       <th><img src='images/person.svg' style='height:70px;width:auto'></th>"+
+        "       <td><span id='person_zeit'></span>" + persontime + " min</td>"+
+        "       <td><span id='person_distance'></span>" + persondist + " km</td>"+
         "   </tr>"+
         "</table>";
 
