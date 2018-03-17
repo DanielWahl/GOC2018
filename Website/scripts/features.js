53163f51c59fa0befc2bc9c26a5621cabff59733
SapoKevin <kevin95matos@gmail.com>
2018-03-17 13:36:23 +0100

    costs

--- Website/scripts/features.js
+++ Website/scripts/features.js
@@ -47,12 +47,16 @@ function loadValues() {
 
         let busdist     = response.bus.distance;
         let bustime     = Math.round(response.bus.time);
+        let busprice    = 2;
         let velohdist   = (response.veloh[1].distance + response.veloh[2].distance + response.veloh[3].distance)/1000;
         let velohtime   = Math.round((response.veloh[1].duration + response.veloh[2].duration + response.veloh[3].duration)/60);
+        let velohprice  = 0;
         let cardist     = response.car[1].distance/1000;
         let cartime     = Math.round(response.car[1].duration/60);
+        let carprice    = 0;
         let persondist  = response.walk[1].distance/1000;
         let persontime  = Math.round(response.walk[1].duration/60);
+        let personprice = 0;
 
         let content = "<span id='testresult'></span>"+
         "<p>"+
@@ -62,21 +66,25 @@ function loadValues() {
         "       <th><img src='images/bus.svg' style='height:auto;width:60px'></th>"+
         "       <td><span id='bus_zeit'></span>" + bustime + " min</td>"+
         "       <td><span id='bus_distance'></span>" + busdist + " km</td>"+
+        "       <td><span id='bus_price'></span>" + busprice + " €</td>"+
         "   </tr>"+
         "   <tr>"+
         "       <th><img src='images/bicycle.svg' style='height:auto;width:60px'></th>"+
         "       <td><span id='veloh_zeit'></span>" + velohtime + " min</td>"+
         "       <td><span id='veloh_distance'></span>" + velohdist + " km</td>"+
+        "       <td><span id='veloh_price'></span>" + velohprice + " €</td>"+
         "   </tr>"+
         "   <tr>"+
         "       <th><img src='images/auto.svg' style='height:auto;width:60px'></th>"+
         "       <td><span id='auto_zeit'></span>" + cartime + " min</td>"+
         "       <td><span id='auto_distance'></span>" + cardist + " km</td>"+
+        "       <td><span id='auto_price'></span>" + carprice + " €</td>"+
         "   </tr>"+
         "   <tr>"+
         "       <th><img src='images/person.svg' style='height:70px;width:auto'></th>"+
         "       <td><span id='person_zeit'></span>" + persontime + " min</td>"+
         "       <td><span id='person_distance'></span>" + persondist + " km</td>"+
+        "       <td><span id='person_price'></span>" + personprice + " €</td>"+
         "   </tr>"+
         "</table>";
 
